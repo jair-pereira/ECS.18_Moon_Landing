d4cb73c1e54be7417d704a5916416b8444ce7dc1
Jair Pereira <jair.pereira@aluno.ufabc.edu.br>
2019-06-17 14:56:33 +0900

    add ponyge

---
+++ src/example_pso.py
@@ -0,0 +1,42 @@
+import numpy as np
+import src
+
+def pso():
+    #instantiate solutions 
+    X = np.array([src.solution(my_func, dimension, bounds) for i in range(n)])
+    #initialize solutions 
+    [Xi.initRandom() for Xi in X]
+    
+    #just so we can have some animations
+    src.solution.updateHistory(X) # it is not necessary for the grammar
+    
+    for it in range(iteration):
+        #1. Select individuals for modification in this round
+        # none - select all. Alternative (bee algorythm) is to select only solutions drawn with fitness-dependant probability
+        #2. de_operator = create an alternative set of solutions X1 using mutation+crossover
+        X1  = src.op.op_pso(X, src.op.select_random, src.op.mut_pso, src.op.crx_exponential)
+        #3. Select individual for the next generation <- accept all
+        X = X1
+        
+        src.solution.updateHistory(X) 
+
+
+    return X
+    
+##param
+n = 30
+iteration = 20
+
+my_func   = sum
+dimension = 3
+bounds    = -5, 5
+
+beta = .5 
+pr = .7
+tournamment = 5
+w = .5 
+c1 = .5 
+c2 = 1
+
+pso()
+print(src.solution.best.getFitness())
---
+++ src/utils.py
@@ -0,0 +1,53 @@
+import numpy as np
+import subprocess
+import os
+
+ALG="FOO"
+RUN=0
+IRACE = False
+modifier = 1
+
+def save_history(x, objs, cons):
+    GEN = 0
+    while os.path.isfile(ALG+"/testrun_mop/{:03}".format(RUN)+"th_run/optimizer/interface/gen{:04}".format(GEN)+"_pop_cons_eval.txt"):
+        GEN = GEN + 1
+    
+    f1 = ALG+"/testrun_mop/{:03}".format(RUN)+"th_run/optimizer/interface/gen{:04}".format(GEN)+"_pop_vars_eval.txt"
+    f2 = ALG+"/testrun_mop/{:03}".format(RUN)+"th_run/optimizer/interface/gen{:04}".format(GEN)+"_pop_objs_eval.txt"
+    f3 = ALG+"/testrun_mop/{:03}".format(RUN)+"th_run/optimizer/interface/gen{:04}".format(GEN)+"_pop_cons_eval.txt"
+    
+    os.makedirs(ALG+"/testrun_mop/{:03}".format(RUN)+"th_run/optimizer/interface", exist_ok=True)
+    
+    np.savetxt(f1, x,    delimiter='	', newline='\n')
+    np.savetxt(f2, objs, delimiter='	', newline='\n')
+    np.savetxt(f3, cons, delimiter='	', newline='\n')
+    
+    return
+    
+def evaluate(x):
+    if os.path.isfile("pop_vars_eval.txt"):
+        os.remove("pop_vars_eval.txt")
+    if os.path.isfile("pop_objs_eval.txt"):
+        os.remove("pop_objs_eval.txt")
+    if os.path.isfile("pop_cons_eval.txt"):
+        os.remove("pop_cons_eval.txt")
+    #write X to a file
+    np.savetxt("pop_vars_eval.txt", x, delimiter='	', newline='\n')
+    
+    #evaluate X
+    subprocess.call(r"moon_sop.exe ./")
+    
+    #load the result of the evaluation
+    objs = np.loadtxt("pop_objs_eval.txt") #maximization of objs
+    cons = np.loadtxt("pop_cons_eval.txt") #constraints: c0<0.05, c1<0.3
+    
+    #history
+    if not IRACE:
+        save_history(x, objs, cons)
+    
+    #penalty
+    c0 = np.array([0 if x[0]<0.05 else x[0] for x in cons])
+    c1 = np.array([0 if x[1]<0.3 else x[1] for x in cons])
+    fit = objs + (c0+c1)*modifier
+    
+    return fit
